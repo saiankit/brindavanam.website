c46ae01a98ce09df2253eeaa4b61abf63dde4e23
Sai Ankit <saiankit30@gmail.com>
2021-10-23 20:38:21 +0530

    First Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 module.exports = {
-  mode: 'jit',
+  mode : "jit",
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
+    container: {
+      center: true
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+  ],
 }
